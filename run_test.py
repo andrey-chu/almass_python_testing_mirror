4ff999e0c8672b04612b70f59f276b2c8417721e
Andrey Chuhutin <end.product@gmail.com>
2020-03-02 15:47:13 +0100

    two first files

---
+++ run_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Binary version of the test file
+Should do the same thing as the jupyter notebook
+Created on Mon Mar  2 14:56:09 2020
+
+@author: au517882
+"""
+data_dir = "~/CLionProjects/GooseTests/run-directory/"
+source_dir = "~/CLionProjects/ALMaSS_all"
+
+
+
