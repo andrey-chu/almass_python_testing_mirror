d04ea55eefcab299f8427aec4e5bae43f8b76984
Andrey Chuhutin <end.product@gmail.com>
2020-06-25 13:30:32 +0200

    working version of bembidion analysis very slow

--- beetle_test1.py
+++ beetle_test1.py
@@ -53,4 +53,28 @@ ax1.set_title('Daily population size')
 stages_columnnames = ['Ignore', 'Year', 'TotalDay', 'DayInYear', 'YearNo', 'State', 'CanReproduce']
 stages_data1=pd.read_csv(results_dir1+"debug.tsv", sep='\t', header=None, names=stages_columnnames, usecols=[2, 4, 5, 6], dtype={'TotalDay':'Int64', 'YearNo':'Int64', 'State':'Int64','CanReproduce':'Int64'}, skiprows=[0])
 stages_data2=pd.read_csv(results_dir2+"debug.tsv", sep='\t', header=None, names=stages_columnnames, usecols=[2, 4, 5, 6], dtype={'TotalDay':'Int64', 'YearNo':'Int64', 'State':'Int64','CanReproduce':'Int64'}, skiprows=[0])
-stages_data1_grouped=stages_data1.groupby(['TotalDay', 'State'])['CanReproduce'].count()
+stages_data1_grouped=stages_data1.groupby(['TotalDay', 'State','CanReproduce']).size()
+stages_data1_dead=stages_data1_grouped[stages_data1_grouped.index.get_level_values(1).isin([14])].reset_index().set_index('TotalDay').iloc[:,2]
+stages_data1_dead.rename(lambda x: x -366)
+stages_data1_dead_frame=stages_data1_dead.to_frame()
+stages_data1_dead_frame['DayDate']=my_dateparser(np.uint(stages_data1_dead_frame.index))
+
+
+stages_data2_grouped=stages_data2.groupby(['TotalDay', 'State','CanReproduce']).size()
+stages_data2_dead=stages_data2_grouped[stages_data2_grouped.index.get_level_values(1).isin([14])].reset_index().set_index('TotalDay').iloc[:,2]
+stages_data2_dead.rename(lambda x: x -366)
+stages_data2_dead_frame=stages_data2_dead.to_frame()
+stages_data2_dead_frame['DayDate']=my_dateparser(np.uint(stages_data2_dead_frame.index))
+
+fig2, ax2 = plt.subplots(figsize=mpl.figure.figaspect(0.5)*2)
+months = mdates.MonthLocator()
+myFmt = mdates.DateFormatter('%b')
+fig2.autofmt_xdate(rotation='vertical')
+ax2.xaxis.set_major_formatter(myFmt)
+ax2.xaxis.set_minor_locator(months)
+ax2.xaxis_date()
+ax2.grid()
+line1=ax2.plot(stages_data1_dead_frame['DayDate'], stages_data1_dead_frame[0], marker=".",  label='orig')
+line2=ax2.plot(stages_data2_dead_frame['DayDate'], stages_data2_dead_frame[0], marker=".",  label='mine')
+ax2.legend( fancybox=True, shadow=True, title='Bembidion Daily death rate', loc='center right',bbox_to_anchor=(1.1, 0.60))
+ax2.set_title('Bembidion model')
