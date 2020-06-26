5caf7f88a754850e935b964be6ab09151865b539
Andrey Chuhutin <end.product@gmail.com>
2020-06-26 17:52:27 +0200

    minor

--- beetle_test1.py
+++ beetle_test1.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib as mpl
 
-simulation_start_date = dt.date(2009, 1, 1)# The year is probably incorrect: check for the right one
+simulation_start_date = dt.date(1991, 1, 1)# The year is probably incorrect: check for the right one
 my_dateparser=(lambda x: pd.to_datetime(x,unit='D', origin=simulation_start_date))
 
 stages_names=['Egg', 'Larva', 'Pupa', 'Adult']
@@ -36,10 +36,10 @@ ax1.xaxis_date()
 ax1.grid()
 time1 = population_data1['Daydate']
 time2 = population_data2['Daydate']
-line1=ax1.plot(time1, population_data1['Egg'], marker="o",  label='Std Egg')
-line2=ax1.plot(time1, population_data1['Larva'], marker="o",  label='Std Larva')
-line3=ax1.plot(time1, population_data1['Pupa'], marker="o",  label='Std Pupa')
-line4=ax1.plot(time1, population_data1['Adult'], marker="o",  label='Std Adult')
+line1=ax1.plot(time1, population_data1['Egg'], marker=None,  label='Std Egg')
+line2=ax1.plot(time1, population_data1['Larva'], marker=None,  label='Std Larva')
+line3=ax1.plot(time1, population_data1['Pupa'], marker=None,  label='Std Pupa')
+line4=ax1.plot(time1, population_data1['Adult'], marker=None,  label='Std Adult')
 line11=ax1.plot(time2, population_data2['Egg'], marker=".", color=line1[0]._color, label='Mine Egg')
 line21=ax1.plot(time2, population_data2['Larva'], marker=".", color=line2[0]._color, label='Mine Larva')
 line31=ax1.plot(time2, population_data2['Pupa'], marker=".", color=line3[0]._color, label='Mine Pupa')
@@ -74,6 +74,8 @@ ax2.xaxis.set_major_formatter(myFmt)
 ax2.xaxis.set_minor_locator(months)
 ax2.xaxis_date()
 ax2.grid()
+
+
 line1=ax2.plot(stages_data1_dead_frame['DayDate'], stages_data1_dead_frame[0], marker=".",  label='orig')
 line2=ax2.plot(stages_data2_dead_frame['DayDate'], stages_data2_dead_frame[0], marker=".",  label='mine')
 ax2.legend( fancybox=True, shadow=True, title='Bembidion Daily death rate', loc='center right',bbox_to_anchor=(1.1, 0.60))
